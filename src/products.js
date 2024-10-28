af74e485b7344f33d7883dffd72eb85067118946
Eulices <48368478+EulicesCahuantzi@users.noreply.github.com>
2024-10-27 21:42:18 -0700

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,52 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone 16 Pro Max",
+    price: 1199.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Iphone 16 Pro",
+    price: 999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Iphone 16 Plus",
+    price: 899.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Iphone 16",
+    price: 799.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Samsung S24 Ultra",
+    price: 1299.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Samsung S24+",
+    price: 999.99,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Samsung S24",
+    price: 859.99,
+    productImage: product7,
+  },
+];
